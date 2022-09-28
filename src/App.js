99cc81056e8828950d2304ce67a6869e4ee1e1a4
sarbanandabhikkhu <sarbanandabhikkhu@gmail.com>
2022-09-28 23:11:57 +0600

    rect tabs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
-import Welcome from "./components/welcome";
-import data from "../public/manifest.json";
 
 function App() {
-  const { short_name, name } = data;
   return (
     <div className="App">
-      <Welcome label={short_name} value={name} />
+      <Tabs />
     </div>
   );
 }
