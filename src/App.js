6e9e2d9869d3cfabafda4764e1b897e777ddb7fc
sarbanandabhikkhu <sarbanandabhikkhu@gmail.com>
2022-09-29 21:51:43 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { hot } from "react-hot-loader/root";
 import Accordion from "./components/accordion";
 import Navbar from "./components/navbar";
+import Tabs from "./components/tabs";
 
 function App() {
   return (
     <div className="App">
       <Accordion />
       <Navbar />
+      <Tabs />
     </div>
   );
 }
